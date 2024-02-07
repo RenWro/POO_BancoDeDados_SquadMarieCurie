72ff5e6acb09e0a7d4fd4af13f612c5edd118acc
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-02-07 00:35:45 -0300

    Update livro.py
    Adicionado o patch da classe autores à classe livro

--- exercicio_POO/livro.py
+++ exercicio_POO/livro.py
@@ -1,8 +1,9 @@
 class Livro:
-    def __init__(self, titulo, editora, lista_generos=None, numero_exemplares=0):
+    def __init__(self, titulo, editora, autores=None, lista_generos=None, numero_exemplares=0):
         self.__titulo = titulo
         self.__editora = editora
-        self.__lista_generos = lista_generos if lista_generos is not None else []
+        self.__autores = autores if autores is not None else []  # Inicializa com uma lista vazia se nenhum autor for fornecido
+        self.__lista_generos = lista_generos if lista_generos is not None else []  # Inicializa com uma lista vazia se nenhum gênero for fornecido
         self._numero_exemplares = numero_exemplares
 
     @property
@@ -29,17 +30,25 @@ class Livro:
     def lista_generos(self, nova_lista_generos):
         self.__lista_generos = nova_lista_generos
 
+    @property
+    def autores(self):
+        return self.__autores
+
+
+    def adicionar_autor(self, autor):
+        if autor not in self.__autores:
+            self.__autores.append(autor)
+            autor.adicionar_livro(self)
+
     def detalhes_livro(self):
         estado = ' ----------------------------------\n'
         estado += f'  Livro: {self.__titulo}\n'
         estado += f'  Editora: {self.__editora}\n'
+        estado += f'  Autor(es): {", ".join([autor.nome for autor in self.__autores])}\n'
         estado += f'  Gênero(s): {", ".join(self.__lista_generos)}\n'
         estado += f'  Número de exemplares: {self._numero_exemplares}\n'
-        estado += f' ----------------------------------\n'
+        estado += ' ----------------------------------\n'
         return estado
 
     def __str__(self):
         return self.detalhes_livro()
-
-mochileiro = Livro('O Guia do Mochileiro das Galáxias', 'Arqueiro', ['Novel', 'Science fiction', 'Humor', 'Science fiction comedy', 'Humorous Fiction'], 5)
-print(mochileiro)
