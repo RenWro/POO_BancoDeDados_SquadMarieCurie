8e061905722276ec3c354d3c6e6e7dab8dde348d
Bea-Martins <bmc_c@outlook.com>
2024-02-08 06:55:52 -0300

    fix: alterado arquivo emprestimono padrão de classe abstrata

--- exercicio_POO/emprestimo.py
+++ exercicio_POO/emprestimo.py
@@ -1,43 +1,17 @@
 from abc import ABC, abstractmethod
 
-
 class Emprestimo(ABC):
-    def _init_(self):
+    def __init__(self):
         self.__emprestimos = {}
 
     @abstractmethod
     def emprestar_livro(self, livro, usuario):
-        if livro.titulo not in self.__emprestimos:
-            if livro._numero_exemplares > 0:
-                livro._numero_exemplares -= 1
-                self.__emprestimos[livro.titulo] = {"usuario": usuario, "renovacoes": 0}
-                print(f"Livro '{livro.titulo}' emprestado para {usuario}.")
-            else:
-                print(f"Livro '{livro.titulo}' não disponível para empréstimo.")
-        else:
-            print(f"Livro '{livro.titulo}' já está emprestado.")
         pass
 
     @abstractmethod
     def devolver_livro(self, livro):
-        if livro.titulo in self.__emprestimos:
-            livro._numero_exemplares += 1
-            del self.__emprestimos[livro.titulo]
-            print(f"Livro '{livro.titulo}' devolvido com sucesso.")
-        else:
-            print(f"Livro '{livro.titulo}' não consta como emprestado.")
-            pass
+        pass
 
     @abstractmethod
     def renovar_emprestimo(self, livro):
-        if livro.titulo in self.__emprestimos:
-            if self.__emprestimos[livro.titulo]["renovacoes"] < 3:
-                self.__emprestimos[livro.titulo]["renovacoes"] += 1
-                print(
-                    f"Empréstimo do livro '{livro.titulo}' renovado. Total de renovações: {self.__emprestimos[livro.titulo]['renovacoes']}."
-                )
-            else:
-                print("Limite de renovações atingido.")
-        else:
-            print(f"Livro '{livro.titulo}' não está emprestado para ser renovado.")
-            pass
+        pass
